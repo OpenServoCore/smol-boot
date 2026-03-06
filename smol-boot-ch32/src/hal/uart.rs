d1255ef83b90913076ff1ecc4034a684bea53368
Aaron Qian <aq1018@gmail.com>
2026-03-05 23:16:57 -0800

    rename Ch32Transport to Ch32UartTransport

--- smol-boot-ch32/src/hal/uart.rs
+++ smol-boot-ch32/src/hal/uart.rs
@@ -2,25 +2,25 @@ use core::convert::Infallible;
 
 use embedded_io::ErrorType;
 
-pub(crate) struct Ch32Transport;
+pub(crate) struct Ch32UartTransport;
 
-impl Ch32Transport {
+impl Ch32UartTransport {
     pub fn new() -> Self {
-        Ch32Transport
+        Ch32UartTransport
     }
 }
 
-impl ErrorType for Ch32Transport {
+impl ErrorType for Ch32UartTransport {
     type Error = Infallible;
 }
 
-impl embedded_io::Read for Ch32Transport {
+impl embedded_io::Read for Ch32UartTransport {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
         todo!()
     }
 }
 
-impl embedded_io::Write for Ch32Transport {
+impl embedded_io::Write for Ch32UartTransport {
     fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
         todo!()
     }
--- smol-boot-ch32/src/lib.rs
+++ smol-boot-ch32/src/lib.rs
@@ -4,9 +4,9 @@ mod hal;
 
 use smol_boot::{Core, hal::Hal};
 
-use hal::{Ch32Abi, Ch32Flash, Ch32Registry, Ch32Transport};
+use hal::{Ch32Abi, Ch32Flash, Ch32Registry, Ch32UartTransport};
 
-type Ch32Core = Core<Ch32Transport, Ch32Flash, Ch32Registry, Ch32Abi>;
+type Ch32Core = Core<Ch32UartTransport, Ch32Flash, Ch32Registry, Ch32Abi>;
 
 pub struct Bootloader {
     core: Ch32Core,
@@ -14,7 +14,7 @@ pub struct Bootloader {
 
 impl Default for Bootloader {
     fn default() -> Self {
-        let transport = Ch32Transport::new();
+        let transport = Ch32UartTransport::new();
         let flash = Ch32Flash::new();
         let abi = Ch32Abi::new();
         let reg = Ch32Registry::new();
